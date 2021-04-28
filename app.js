5ce1a91e4af9bef60988f603ffe6cdafffcd37ff
Tamaza <igrigolia@wpi.edu>
2021-04-28 14:04:44 +0400

    make it better

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
 
+let port = process.env.PORT;
+if(port == null || port ==""){
+  port = 3000;
+}
 app.listen(3000, function(){
     console.log("server started...");
   });
